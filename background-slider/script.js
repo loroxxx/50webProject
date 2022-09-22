1f0f2c4d68a231ef2b0e1a38aea38a08a2114232
yuanqiang.luo <yuanqiang.luo>
2022-09-22 17:59:40 +0800

    222

--- background-slider/script.js
+++ background-slider/script.js
@@ -3,7 +3,6 @@ const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
-
 let activeSlide = 0
 
 rightBtn.addEventListener('click', () => {
